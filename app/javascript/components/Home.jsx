3bec20dacf1b6a1a09af71c92c114af2f699b645
kenyounot123 <kenlu519@gmail.com>
2024-05-18 00:02:39 -0400

    Initialize React components and update routes

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="">
+    <div className="">
+      <div className="">
+        <h1 className="">Food Recipes</h1>
+        <p className="">
+          A curated list of recipes for the best homemade meal and delicacies.
+        </p>
+        <hr className="" />
+      </div>
+    </div>
+  </div>
+);
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+def generate_time_record_string
+  time = Faker::Time.backward(days: rand(365))
+  milliseconds = rand(1000) # Generate random milliseconds
+  time.strftime("%M:%S:") + milliseconds.to_s.rjust(2, '0') # Pad milliseconds with zeros
+end
+
+User.delete_all
+# Generate 5 fake users with names and time records
+5.times do
+  User.create(
+    name: Faker::Name.name,
+    record: generate_time_record_string
+  )
+end
