36f389861cedc535c06759400dbcabb7e7a82b2f
kenyounot123 <kenlu519@gmail.com>
2024-05-18 18:07:57 -0400

    Add dark mode function

--- app/javascript/components/index.jsx
+++ app/javascript/components/index.jsx
@@ -1,18 +1,40 @@
 import React from "react";
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import "../../assets/stylesheets/application.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../routes/Home/Home";
+import Game from "../routes/Game/Game";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  {
+    path: "/play",
+    element: <Game />,
+  },
 ]);
+
+const App = () => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <div className={"app"} data-theme={theme}>
+      <button onClick={toggleTheme}>Toggle Dark Mode</button>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
 document.addEventListener("turbo:load", () => {
   const root = createRoot(
     document.body.appendChild(document.createElement("div"))
   );
-  root.render(<RouterProvider router={router} />);
+  root.render(<App />);
 });
